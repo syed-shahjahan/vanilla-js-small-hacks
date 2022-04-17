d9fc4b3972c59ac8c15b541632fd85e00194f251
Syed Shahjahan <42941326+syed-shahjahan@users.noreply.github.com>
2022-04-17 20:16:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+var SEPARATOR = "/";
+
+document.getElementById(
+  "app"
+).innerHTML = `<h1 class="text-center">Feature Branch Name Builder</h1>
+<div id="callout"></div>
+<div id="form-container"></div>
+`;
